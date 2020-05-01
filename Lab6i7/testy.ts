e4e81bb75b78ddd12d992b54cf8e042a8ef10e64
„Piotr <pn406263@students.mimuw.edu.pl>
2020-05-01 17:13:03 +0200

    added more webiste tests

--- Lab6i7/testy.ts
+++ Lab6i7/testy.ts
@@ -26,13 +26,64 @@ describe("Fibonacci", () => {
     });
 }); 
 
+function getCurrentDate() : string {
+    return new Date().toISOString().slice(0, 10);
+}
+
+const path = `file://${process.cwd()}/strona.html`;
+const myBirthday = '1999-05-22';
+const currentDate = getCurrentDate();
+const nextYear = ((new Date).getFullYear() + 1) + "-05-22";
+const fullName = 'Piotr Nawrot';
+
 describe('DateInForm', function () {
     it('form should be blocked with date smaller than current', async function() {
-        await driver.get(`file://${process.cwd()}/strona.html`);
-        const myBirthday = '1999-05-22';
+        await driver.get(path);
         const dateInput = await driver.find('input[type=date]');
         driver.executeScript('arguments[0].value = arguments[1]', dateInput, myBirthday);
 
+        const nameInput = await driver.find('input[name=name]');
+        nameInput.sendKeys(fullName);
+
         expect(await (await driver.find('input[type=submit]')).isDisplayed()).to.equal(false);
+    });
+});
+
+describe('validSubmit1', function () {
+    it('wrong name or surname, only one word in the field', async function() {
+        await driver.get(path);
+        const dateInput = await driver.find('input[type=date]');
+        const nameInput = await driver.find('input[name=name]');
+
+        driver.executeScript('arguments[0].value = arguments[1]', dateInput, nextYear);
+        driver.executeScript('arguments[0].value = arguments[1]', nameInput, 'word');
+
+        expect(await (await driver.find('input[type=submit]')).isDisplayed()).to.equal(false);
+    });
+});
+
+describe('validSubmit2', function () {
+    it('missing name and surname', async function() {
+        await driver.get(path);
+        const dateInput = await driver.find('input[type=date]');
+        const nameInput = await driver.find('input[name=name]');
+
+        nameInput.sendKeys('');
+        driver.executeScript('arguments[0].value = arguments[1]', dateInput, nextYear);
+
+        expect(await (await driver.find('input[type=submit]')).isDisplayed()).to.equal(false);
+    });
+});
+
+describe('validSubmit3', function () {
+    it('form is ok so submit should be displayed', async function() {
+        await driver.get(path);
+        const dateInput = await driver.find('input[type=date]');
+        const nameInput = await driver.find('input[name=name]');
+
+        nameInput.sendKeys(fullName);
+        driver.executeScript('arguments[0].value = arguments[1]', dateInput, nextYear);
+
+        expect(await (await driver.find('input[type=submit]')).isDisplayed()).to.equal(true);
     });
 });
